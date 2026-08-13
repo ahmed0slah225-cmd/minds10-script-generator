e606313cbdc1ce0f84938836fe6360d4cd5649d7
ahmed0slah225-cmd <ahmed0slah225@gmail.com>
2026-08-13 18:53:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ if st.button("🚀 ابدأ المعالجة عبر الـ 10 عقول", type="p
                 """
 
                 response = client.models.generate_content(
-                    model="gemini-2.5-flash",
+                    model="gemini-1.5-flash",
                     contents=prompt_text
                 )
 
