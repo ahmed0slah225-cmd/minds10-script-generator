9c40c2ff2055a451a88594bad123821f29665c0a
ahmed0slah225-cmd <ahmed0slah225@gmail.com>
2026-09-10 23:06:57 +0300

    Delete ui directory

--- ui/layout.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from config.settings import get_settings
-from ui.styles import inject
-from ui.navigation import nav
-
-def shell():
- st.set_page_config(page_title='Minds10',page_icon='🎬',layout='wide')
- inject(); st.sidebar.markdown('## 🎬 Minds10'); st.sidebar.caption('Content Intelligence Platform'); return nav()
