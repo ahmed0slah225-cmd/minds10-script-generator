876506a265d26a3051e514b5f83afbc963b1ce1d
ahmed0slah225-cmd <ahmed0slah225@gmail.com>
2026-09-11 22:35:54 +0300

    Delete tests directory

--- tests/test_imports.py
+++
@@ -1,6 +0,0 @@
-def test_core_imports():
-    import config
-    from core.models import Project, PipelineState
-    from engines.pipeline import Pipeline
-    assert config.APP_TITLE
-    assert Project and PipelineState and Pipeline
