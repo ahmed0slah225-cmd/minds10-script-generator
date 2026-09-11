c2982eacf9ec242e5ff36e416fa73175a0a251b8
ahmed0slah225-cmd <ahmed0slah225@gmail.com>
2026-09-11 22:36:14 +0300

    Delete skills directory

--- skills/voice_dna_ar_eg/rules.py
+++
@@ -1,5 +0,0 @@
-FIELDS = [
-    "sentence_length", "rhythm", "questions", "vocabulary", "colloquiality", "emotion",
-    "examples", "transitions", "curiosity", "idea_endings", "analogies", "formality",
-    "spontaneity", "audience_address"
-]
